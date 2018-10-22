4978ff6b1cb35e242e5e220136fa27e713d13eb0
Tony Nyarangi <antony.momanyi@gmail.com>
2018-10-22 13:51:11 +0300

    transaction logic

---
+++ farmcoin/logic.js
@@ -0,0 +1,28 @@
+//cashTransfer
+function cashTransfer(cashTransfer){
+    if(cashTransfer.from.balance < cashTransfer.amount){
+        console.log("Insufficient balance");
+    }else{
+        cashTransfer.from.balance -= cashTransfer.amount;
+        cashTransfer.to.balance += cashTransfer.amount;
+    }
+}
+//requestForProduce
+//landRegistration
+function landRegistration(landRegistration, newLand){
+    landRegistration.newLand.LRnumber = newLand.landRegistrationNumber;
+    landRegistration.newLand.owner = newLand.landOwner;
+}
+//registerFarmer
+//registerGovernmentAgency
+//registerBank
+//registerLandRegistrar
+//convertCoinsToCash
+function convertCoinsToCash(coins){
+    let cashValue = coins*100;
+    return cashValue;    
+}
+//payout
+function payout(){
+
+}
